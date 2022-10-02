fbe46bb8eeae9634a90d35cf9b469dfe5c16d850
Douglas <douglasobara97@gmail.com>
2022-10-02 06:26:57 +0300

    use get http verb

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -1,2 +1,33 @@
 class AccountController < ApplicationController
+    def index
+        accounts = Account.all
+        render json: {
+            status: 200,
+            message: "OK",
+            data: {
+                accounts: accounts
+            }
+        }, status: :ok
+    end
+
+    def show
+        account = Account.find_by(number: params[:acc_number ])
+        if !account
+            render json: {
+                status: 404,
+                message: "Missing Account",
+                data: {
+                    account: account
+                }
+            }, status: :ok
+        else
+            render json: {
+                status: 200,
+                message: "OK",
+            data: {
+                    account: account
+                }
+            }
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/accounts', to: "account#index"
+  get '/accounts/:acc_number', to: "account#show"
 end
